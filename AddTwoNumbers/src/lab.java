20cb2e52d9df1dd690336326003d91e4ec368be1
hi <hi@DESKTOP-0HDJ7TB>
2021-06-06 16:38:12 +0530

    first commit

---
+++ AddTwoNumbers/src/lab.java
@@ -0,0 +1,12 @@
+
+public class lab {
+
+	public static void main(String[] args) {
+		int number1=50,number2=150,sum;
+		sum=number1+number2;
+		System.out.println("sum of these numbers: "+sum);
+	
+		
+	}
+
+}
